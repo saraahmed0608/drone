39d5dca08d8fc5f23da9430012680bb1042a0608
V3490 (Conference ID) <v3490@cam.ac.uk>
2015-08-14 11:05:24 +0100

    try 1

--- drone-backend.js
+++ drone-backend.js
@@ -1,5 +1,4 @@
 var Cylon = require('cylon');
-var bot;
 
 // Initialise the robot
 Cylon.robot()
@@ -11,11 +10,88 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"               //put second device on
+    })
     .on("ready", fly);
-    
+
+var bot;
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
 
+    bot.nav.on("navdata", function(data) {
+        // console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+               if (altitude > 2.0) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    // Stop any emergency setting
+    bot.drone.disableEmergency();
+    // Tell the drone it is lying flat
+    bot.drone.ftrim();
+
+    // Take off
+    bot.drone.takeoff();
+    after(5*1000, function() {
+        bot.drone.takeoff(0);
+    });
+
+
+    bot.drone.left(0.3);
+    after(10*1000, function() {
+        bot.drone.left(0);
+    });
+
+    bot.drone.right(0.3);
+    after(15*1000, function() {
+        bot.drone.right(0);
+    });
+
+    after(25*1000, function() {
+        bot.drone.land();
+    });
+    after(37*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
